61dc00d5eb724a5a7bc432ee23c2a1a8cdbd6152
Danyal <danyalkhan8271@gmail.com>
2023-08-17 13:17:36 +0530

    object & arrays

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,30 @@
-var character = "Iron-Man";
-var age = 24;
-var isBlackbelt = true;
-// age = "Thirty two"
-// isBlackbelt = 23;
-isBlackbelt = false;
-age = 32;
-//  circumference function'
-var circ = function (diameter) {
-  return diameter * Math.PI;
+var names = ["Batman", "Flash", "Aquaman"]; // only string
+// names.push(34);
+names.push("Superman");
+// names[1] = 25;
+var numbers = [12, 22, 34, 45]; // only numbers
+numbers.push(11);
+// numbers.push("abc");
+// numbers[0] = "abc";
+var mixed = ["Batman", 22, 34, "Flash", true]; // it takes the data-type of when variable is declared, can't change data-type after declaration
+mixed.push("ABc");
+mixed.push(25);
+mixed.push(false);
+// objects
+var ninja = {
+    name: "Groot",
+    belt: "white",
+    age: 17
+    // we can't change the data-type of these variables
+    // once we define the object we can't add new properties into it
 };
-
-console.log(circ(15));
+ninja.name = "Thor";
+ninja.age = 1500;
+// ninja.belt = 25
+// ninja.name = 500
+ninja = {
+    name: "Flash",
+    belt: "orange",
+    age: 22
+    // if we miss some properties to enter it's check with the initial structure and copmpare and give error
+};
